d0c921680ff5ba4c503c297eb86370303e3623fa
Ludovic Muller <ludovicmuller1@gmail.com>
2022-01-27 23:38:58 +0100

    generate new session

--- vonage/pages/api/session/[name].ts
+++ vonage/pages/api/session/[name].ts
@@ -1,4 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import OpenTok from 'opentok';
+
+// fetch values from environment variables
+const apiKey = process.env.VONAGE_API_KEY || "apiKey";
+const apiSecret = process.env.VONAGE_API_SECRET || "apiSecret";
+const opentok = new OpenTok(apiKey, apiSecret);
+
+const newSession = async (): Promise<string> => {
+  const sessionId: string = await new Promise((resolve, reject) => {
+    opentok.createSession({
+      archiveMode: 'manual',
+    }, (err, session) => {
+      // if there was an error, return it, and exit
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      // if no session ID was returned, exit
+      if (!session?.sessionId) {
+        reject("no session ID");
+        return;
+      }
+
+      resolve(session.sessionId);
+    });
+  });
+
+  return sessionId;
+};
 
 type Data = {
   name: string,
@@ -7,14 +37,13 @@ type Data = {
 
 const sessions = new Map();
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   const name = req.query?.name as string;
-  const d = new Date();
   if (!sessions.has(name)) {
-    sessions.set(name, d.toISOString());
+    sessions.set(name, await newSession());
   }
   const sessionId = sessions.get(name);
   res.status(200).json({ name, id: sessionId });
